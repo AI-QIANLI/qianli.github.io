1286f7876a6668114b121742068f7ba584f8c25c
AI-QIANLI <141759404+AI-QIANLI@users.noreply.github.com>
2023-08-12 00:38:18 +0800

    Add files via upload

---
+++ js/snowfall.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($) {
+	$('body').wpSuperSnow({
+		flakes: ['img/1.png', 'img/1.png', 'img/1.png', 'img/1.png', 'img/1.png', 'img/1.png', 'img/1.png', 'img/1.png'],
+		totalFlakes: '90',
+		zIndex: '999999',
+		maxSize: '20',
+		maxDuration: '20',
+		useFlakeTrans: false
+	})
+});
